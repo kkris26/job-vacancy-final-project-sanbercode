b5a9383de5c5f86a648656daeb5c5a54300f1aca
kkris26 <krisnuartha09@gmail.com>
2025-04-19 22:21:39 +0800

    Specify Site's URL in Vite Config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "https://sanbercode.krisnu.com/",
   plugins: [tailwindcss(), react()],
 });
